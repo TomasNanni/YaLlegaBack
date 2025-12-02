14a3dee8c3f3f5fde937522c3a2912ad63e757b9
TomasNanni <tomasnanni18@gmail.com>
2025-12-02 00:28:01 -0300

    hecho restaurante repo

--- YaLlegaBack/Repositories/RestaurantRepostory.cs
+++ YaLlegaBack/Repositories/RestaurantRepostory.cs
@@ -1,38 +1,59 @@
-﻿using YaLlega.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using YaLlega.Entities;
+using YaLlegaBack.Data;
 using YaLlegaBack.Interfaces;
 
 namespace YaLlegaBack.Repositories
 {
     public class RestaurantRepostory : IRestaurantRepository
     {
+        private YaLlegaBackContext _context;
+
+        public RestaurantRepostory(YaLlegaBackContext context)
+        {
+            _context = context;
+        }
         public bool CheckIfRestaurantExists(int restaurantId)
         {
-            throw new NotImplementedException();
+            return _context.Restaurants.Any(restaurant=> restaurant.Id == restaurantId);
         }
 
         public int Create(Restaurant newRestaurant)
         {
-            throw new NotImplementedException();
+            var createdRestaurant = _context.Restaurants.Add(newRestaurant).Entity;
+            _context.SaveChanges();
+            return createdRestaurant.Id;
         }
 
         public void Delete(int restaurantId)
         {
-            throw new NotImplementedException();
+            _context.Restaurants.Remove(_context.Restaurants.Single(restaurant => restaurant.Id == restaurantId));
+            _context.SaveChanges();
+            return;
         }
 
         public List<Restaurant> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Restaurants.ToList();
         }
 
         public Restaurant? GetById(int restaurantId)
         {
-            throw new NotImplementedException();
+            return _context.Restaurants.FirstOrDefault(restaurant => restaurant.Id == restaurantId);
         }
 
         public void Update(Restaurant updatedRestaurant, int restaurantId)
         {
-            throw new NotImplementedException();
+            var restaurantToEdit = _context.Restaurants.First(u => u.Id == restaurantId);
+            restaurantToEdit.Name = updatedRestaurant.Name;
+            restaurantToEdit.UrlLogoImage = updatedRestaurant.UrlLogoImage;
+            restaurantToEdit.UrlBannerImage = updatedRestaurant.UrlBannerImage;
+            restaurantToEdit.OpenDays = updatedRestaurant.OpenDays;
+            restaurantToEdit.OpeningTime = updatedRestaurant.OpeningTime;
+            restaurantToEdit.ClosingTime = updatedRestaurant.ClosingTime;
+            restaurantToEdit.Contact = updatedRestaurant.Contact;
+            _context.SaveChanges();
+            return;
         }
     }
 }
